e9549950d37667552c7b3fdfb675c58026117488
ljt990218 <2511590093@qq.com>
2022-06-02 17:18:12 +0800

    旋转数组 解法++

--- rotate-array.js
+++ rotate-array.js
@@ -1,12 +1,28 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var nums = [1, 2, 3, 4, 5, 6, 7];
 var k = 3;
-var rotArr = function (nums, k) {
-    for (var i = 0; i < k; i++) {
-        var item = nums[nums.length - 1];
-        console.log(item);
-        nums.pop();
-        nums.unshift(item);
-    }
-    return nums;
+// 临时存储最后一个 => 删除最后一个 => 添加到最前
+// const rotArr = (nums: any, k: number) => {
+//     for (let i = 0; i < k; i++) {
+//         let item = nums[nums.length - 1]
+//         nums.pop()
+//         nums.unshift(item)
+//     }
+//     return nums
+// }
+// console.log(rotArr(nums, k))
+// 删除前 k 个元素 => 向数组前加 k 个元素
+var rotArr2 = function (nums, k) {
+    console.log(nums.slice(nums.length - k, nums.length));
+    console.log(nums.slice(0, nums.length - k));
+    return __spreadArray(__spreadArray([], nums.slice(nums.length - k, nums.length), true), nums.slice(0, nums.length - k), true);
 };
-console.log(rotArr(nums, k));
+console.log(rotArr2(nums, k));
--- rotate-array.ts
+++ rotate-array.ts
@@ -1,14 +1,23 @@
 let nums = [1, 2, 3, 4, 5, 6, 7]
 let k = 3
 
-const rotArr = (nums: any, k: number) => {
-    for (let i = 0; i < k; i++) {
-        let item = nums[nums.length - 1]
-        nums.pop()
-        nums.unshift(item)
-    }
+// 临时存储最后一个 => 删除最后一个 => 添加到最前
+// const rotArr = (nums: any, k: number) => {
+//     for (let i = 0; i < k; i++) {
+//         let item = nums[nums.length - 1]
+//         nums.pop()
+//         nums.unshift(item)
+//     }
 
-    return nums
+//     return nums
+// }
+// console.log(rotArr(nums, k))
+
+// (0-k) + (k-nums.length)
+const rotArr2 = (nums: any, k: number) => {
+    // console.log(nums.slice(nums.length - k, nums.length))
+    // console.log(nums.slice(0, nums.length - k))
+    return [...nums.slice(nums.length - k, nums.length), ...nums.slice(0, nums.length - k)]
 }
 
-console.log(rotArr(nums, k))
+// console.log(rotArr2(nums, k))
